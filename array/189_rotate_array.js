59b10047bba509873d076acb2ed7eccbda67673a
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2024-07-30 10:50:25 +0300

    solve array problems

---
+++ array/189_rotate_array.js
@@ -0,0 +1,21 @@
+var rotate = function (nums, k) {
+  if (k === nums.length) return nums;
+  let n = k;
+  
+  if (k > nums.length) {
+    n = k % nums.length;
+  }
+  const res = [];
+  for (let i = nums.length - n; i < nums.length; i++) {
+    res.push(nums[i]);
+  }
+  for (let i = 0; i < nums.length - n; i++) {
+    res.push(nums[i]);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = res[i];
+  }
+  return nums;
+};
+
+rotate([1, 2, 3, 4, 5, 6, 7], 3);
