f1207a98c16ee9126f9f8c534f520657fa54822b
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2024-08-06 13:14:16 +0300

    solve problems

---
+++ array/169_majority_element.js
@@ -0,0 +1,14 @@
+const majorityElement = function (nums) {
+  const n = Math.floor(nums.length / 2);
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]]) {
+      hash[nums[i]] = hash[nums[i]] + 1;
+    } else {
+      hash[nums[i]] = 1;
+    }
+    if (hash[nums[i]] > n) return nums[i];
+  }
+};
+
+majorityElement([2, 2, 1, 1, 1, 2, 2]); //2
