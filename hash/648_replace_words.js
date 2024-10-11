e8c1a0c47e1177a87776913cad50c44c4187c630
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2024-10-11 21:10:15 +0300

    solve problem

---
+++ hash/648_replace_words.js
@@ -0,0 +1,8 @@
+const replaceWords = function (dictionary, sentence) {
+  const map = new Map();
+	for (let i = 0; i < dictionary.length; i++) {
+		
+	}
+};
+
+replaceWords(["cat", "bat", "rat"], "the cattle was rattled by the battery");
