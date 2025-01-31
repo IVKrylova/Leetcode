ccdd835dc43a75db52b22e881fbe2c18bba68bec
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2025-01-31 10:57:41 +0300

    solve problem

---
+++ 103_binary_tree_zigzag_level_order_traversal.js
@@ -0,0 +1,52 @@
+// 103. Binary Tree Zigzag Level Order Traversal
+// Tree, Breadth-First Search, Binary Tree
+
+// Level - Medium
+
+// Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from
+// left to right, then right to left for the next level and alternate between).
+
+// Example 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+
+// Example 2:
+// Input: root = [1]
+// Output: [[1]]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 2000].
+// -100 <= Node.val <= 100
+
+const zigzagLevelOrder = function (root) {
+  if (!root) return [];
+  const res = [];
+  let direction = true;
+  const queue = [root];
+  while (queue.length > 0) {
+    const nodes = [];
+    const current = [];
+    while (queue.length > 0) {
+      const node = queue.shift();
+      nodes.push(node);
+    }
+    while (nodes.length > 0) {
+      const node = nodes.shift();
+      current.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    if (direction) {
+      direction = false;
+    } else {
+      direction = true;
+      current.reverse();
+    }
+    res.push(current);
+  }
+  return res;
+};
