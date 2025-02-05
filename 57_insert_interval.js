2103f7644487f54a2580cb20c20afcbff1707cfc
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2025-02-05 15:23:22 +0300

    solve problem

---
+++ 57_insert_interval.js
@@ -0,0 +1,80 @@
+// 57. Insert Interval
+// Array
+
+// Level - Medium
+// Topics
+// Companies
+// Hint
+// You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent
+// the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also
+// given an interval newInterval = [start, end] that represents the start and end of another interval.
+// Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals
+// still does not have any overlapping intervals (merge overlapping intervals if necessary).
+// Return intervals after the insertion.
+// Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+// Example 1:
+// Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+// Output: [[1,5],[6,9]]
+
+// Example 2:
+// Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+// Output: [[1,2],[3,10],[12,16]]
+// Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+// Constraints:
+// 0 <= intervals.length <= 10^4
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 10^5
+// intervals is sorted by starti in ascending order.
+// newInterval.length == 2
+// 0 <= start <= end <= 10^5
+
+const insert = function (intervals, newInterval) {
+  const res = [];
+  let i = 0;
+  while (intervals[i] && intervals[i][1] < newInterval[0]) {
+    res.push(intervals[i]);
+    i++;
+  }
+  if (!intervals[i]) {
+    res.push(newInterval);
+  } else if (newInterval[0] <= intervals[i][0]) {
+    const curInt = [newInterval[0]];
+    while (intervals[i] && intervals[i][0] <= newInterval[1]) {
+      i++;
+    }
+    curInt.push(
+      intervals[i - 1]
+        ? Math.max(intervals[i - 1][1], newInterval[1])
+        : newInterval[1]
+    );
+    res.push(curInt);
+    while (i < intervals.length) {
+      res.push(intervals[i]);
+      i++;
+    }
+  } else if (intervals[i][0] > newInterval[1]) {
+    res.push(newInterval);
+    while (i < intervals.length) {
+      res.push(intervals[i]);
+      i++;
+    }
+  } else if (
+    newInterval[0] >= intervals[i][0] &&
+    newInterval[0] <= intervals[i][1]
+  ) {
+    const curInt = [intervals[i][0]];
+    while (intervals[i] && intervals[i][0] <= newInterval[1]) {
+      i++;
+    }
+    curInt.push(Math.max(intervals[i - 1][1], newInterval[1]));
+    res.push(curInt);
+    while (i < intervals.length) {
+      res.push(intervals[i]);
+      i++;
+    }
+  }
+
+  return res;
+};
