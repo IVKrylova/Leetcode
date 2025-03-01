5783ad33038826adfbb86a132254cd2c95f2afd7
Irina Krylova <70285037+IVKrylova@users.noreply.github.com>
2025-03-01 23:47:42 +0300

    solve problems

---
+++ 441_arranging_coins.js
@@ -0,0 +1,35 @@
+// 441. Arranging Coins
+// Math, Binary Search
+
+// Level - Easy
+
+// You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row
+// has exactly i coins. The last row of the staircase may be incomplete.
+// Given the integer n, return the number of complete rows of the staircase you will build.
+
+// Example 1:
+// Input: n = 5
+// Output: 2
+// Explanation: Because the 3rd row is incomplete, we return 2.
+
+// Example 2:
+// Input: n = 8
+// Output: 3
+// Explanation: Because the 4th row is incomplete, we return 3.
+
+// Constraints:
+// 1 <= n <= 2^31 - 1
+
+const arrangeCoins = function (n) {
+  let count = 0;
+  let rest = n;
+  let step = 1;
+  while (rest > 0) {
+    rest -= step;
+    if (rest >= 0) {
+      count++;
+    }
+    step++;
+  }
+  return count;
+};
